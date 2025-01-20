cb59938a1ed56574771deaf7458c62262d953bd8
shashvatsingh001 <singhshashvat4@gmail.com>
2025-01-20 16:20:51 +0530

    Squashed commit of the following:
    commit b383e8ff41eba55cdc669316d0848381f28bdb0a
    Merge: 2cce4de 4d5eac2
    Author: shashvatsingh001 <singhshashvat4@gmail.com>
    Date:   Mon Jan 20 16:20:28 2025 +0530
    
        Merge branch 'myspace' into develop
    
    commit 2cce4deed96f7d01d80d0fa538f77c53f0b8cbc8
    Author: shashvatsingh001 <singhshashvat4@gmail.com>
    Date:   Mon Jan 20 16:20:19 2025 +0530
    
        Update project.pbxproj
    
    commit ef5953703afa291b12d736847259cb3cbab65ef2
    Merge: e048c6e b412126
    Author: nikhilgupta0 <118151222+nikhilgupta0@users.noreply.github.com>
    Date:   Mon Jan 20 16:15:19 2025 +0530
    
        Merge branch 'ScanDiagnoseNikhil' into develop
    
    commit b412126bc9b722ca67a64f61e0b3591215b96e3a
    Author: nikhilgupta0 <118151222+nikhilgupta0@users.noreply.github.com>
    Date:   Mon Jan 20 16:14:51 2025 +0530
    
        diagnosis page modified
    
    commit e048c6efc99922abebc8495cb162f8acc0050c3a
    Merge: b4afa49 b232103
    Author: Madhav <madhavsxena@gmail.com>
    Date:   Mon Jan 20 12:58:08 2025 +0530
    
        Merge branch 'exploreMadhav' into develop
    
    commit b2321036cf22b7365ad0115cd7de59bd147094de
    Author: Madhav <madhavsxena@gmail.com>
    Date:   Mon Jan 20 12:57:29 2025 +0530
    
        Add floating button(PlantarAI) and make clickable screen for cards is ready though it  is still not clickable
    
    commit fe8da65c9cb1049c5ab6cd161619b76e7cc23b97
    Author: Madhav <madhavsxena@gmail.com>
    Date:   Thu Jan 16 16:49:38 2025 +0530
    
        Update 2
    
    commit 77d55900b013e8a358bf4f992165836640e5a044
    Author: Madhav <madhavsxena@gmail.com>
    Date:   Thu Jan 16 16:49:06 2025 +0530
    
        Update

--- GardenGuruXcode/GardenGuruXcode/Nikhil's work/Diagnosis/diagnosisDataModel.swift
+++ GardenGuruXcode/GardenGuruXcode/Nikhil's work/Diagnosis/diagnosisDataModel.swift
@@ -23,12 +23,12 @@ class DiagnosisScreen{
                                                                          nickName: "Near Sofa",
                                                                          diagnosis: "UnderWatered",
                                                                          sectionDetails: [
-                                                                             "Cure and Treatment": ["Water the plant twice a week.", "Ensure proper drainage."],
+                                                                             "Cure and Treatment": ["Here's a step-by-step process to help you revive underwatered Parlor Palm..", "Gently water the plant.", "Water the plant thoroughly until water drains out from the bottom of the pot.", "Prune Yellow , crispy leaves, as they’re unlikely to recover. Trimming helps plant redirect energy to healthy parts", "Encourage users to water the plant consistently, checking the soil’s top inch for moisture before watering again."],
                                                                              "Preventive Measures": ["Maintain consistent soil moisture.", "Avoid overwatering."],
                                                                              "Symptoms": ["Brown tips on leaves.", "Dry soil."],
                                                                              "Vitamins Required": ["Nitrogen", "Phosphorus", "Potassium"],
                                                                              "Related Images": ["Image 1 Description", "Image 2 Description"],
-                                                                             "Video Solution": ["Video Link 1", "Video Link 2"]
+                                                                             "Video Solution": ["https://youtu.be/BS4WRba4KWI?si=xdm_ZEgharndDdpy", "https://youtu.be/MiepVwi_d74?si=74l95b9ajG8mE4zF"]
                                                                     ])
                                                      ]
     
---
+++ GardenGuruXcode/GardenGuruXcode/Nikhil's work/setReminderViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SetReminderViewController.swift
+//  GardenGuruXcode
+//
+//  Created by Nikhil Gupta on 17/01/25.
+//
+
+import UIKit
+
+class SetReminderViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+   
+    
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(hex: "#E8F3E9") // Light green background
+        
+       
+    }
+    
+ 
+    
+}
+
+
+
